26829b6a611097359212073027ebd866936c773e
irrationnelle <drakkarverenis@gmail.com>
2016-10-10 16:17:07 +0900

    Update Test.java
    05 5th commit on github

--- GitTest/src/gittest/Test.java
+++ GitTest/src/gittest/Test.java
@@ -3,7 +3,7 @@ package gittest;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("Hello, Heejung!");
+		System.out.println("Hello, Rase!");
 	}
 
 }
